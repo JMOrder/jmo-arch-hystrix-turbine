7f6c8eb7a79cc5133ecb33733fe981f940b67c22
Ro Lee <rolee0429@gmail.com>
2020-09-15 08:27:41 -0700

    Initial Commit

---
+++ src/test/java/com/jmorder/jmoarchhystrixturbine/JmoArchHystrixTurbineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmorder.jmoarchhystrixturbine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmoArchHystrixTurbineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
